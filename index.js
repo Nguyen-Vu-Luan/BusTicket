d09196aeffe6ad24299f8866474cc5b35e800243
Nguyen-Vu-Luan <136339196+Nguyen-Vu-Luan@users.noreply.github.com>
2023-08-15 15:16:28 +0700

    deploy
    text

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ window.addEventListener("scroll", () => {
         toTop.classList.remove("active");
     }
 })
+
+//chỉnh ngày hiện tại
+var Date = new Date();
+      
+    // Ngày hiện tại
+    var curDay = curDate.getDate();
+ 
+    // Tháng hiện tại
+    var curMonth = curDate.getMonth() + 1;
+      
+    // Năm hiện tại
+    var curYear = curDate.getFullYear();
+ 
+    // Gán vào thẻ HTML
+    document.getElementById('date').innerHTML = curDay + "/" + curMonth + "/" + curYear;
